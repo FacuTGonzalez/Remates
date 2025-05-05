e4fe1243c884cfb6ce33984f17429d54d7ae791c
FacuTGonzalez <facugonzalezz2020@gmail.com>
2025-05-05 00:08:30 -0300

    Create repository

---
+++ src/app/login/_components/LoginLayout/LoginLayout.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '@/components/modules/forms/LoginForm/LoginForm'
+import React from 'react'
+
+export const LoginLayout = () => {
+  return (
+    <div>
+        <LoginForm/>
+    </div>
+  )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LoginLayout } from './_components/LoginLayout/LoginLayout';
+
+ const Login = () => {
+  return (
+    <LoginLayout/>
+  )
+}
+
+export default Login;
